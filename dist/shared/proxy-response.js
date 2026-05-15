f4280446fd061e60094f6ce7519e0c5deeae8d4c
Angel Szymczak <contact@angelpixel.io>
2026-05-15 16:11:51 -0300

    chore: remove generated api-gateway artifacts from git

--- dist/shared/proxy-response.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.sendProxyResponse = sendProxyResponse;
-function sendProxyResponse(response, status, body) {
-    response.status(status).send(body);
-}
